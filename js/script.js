3105ba18a489f0b4e3ea9256ebcea534c8acbedf
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2023-03-02 08:39:13 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.mobile-menu').onclick = function() {
+    document.querySelector('.mobile-menu').classList.toggle('active');
+    document.querySelector('.nav-list').classList.toggle('active');
+    document.querySelector('.navMobile').classList.toggle('openNav');
+  };
+

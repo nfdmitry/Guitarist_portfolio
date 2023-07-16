2f8c14458e0d0c4de68085b9a88433297766bcb8
Dmitry Nefedov <104081901+nfdmitry@users.noreply.github.com>
2023-07-16 10:53:31 +1000

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-
-const toggleMenu = () => {
-    document.querySelector('.mobile-menu').classList.toggle('active');
-    document.querySelector('.nav-list').classList.toggle('active');
-    document.querySelector('.navMobile').classList.toggle('openNav');
-};
-
-mobileMenu.addEventListener('click', toggleMenu);
